824b67e16ed8a559965af9197fbcfb2fd0f114be
Hofled <ofri.herold@gmail.com>
2022-04-17 18:48:32 +0300

    Updated color of the moon

--- physics/planets.go
+++ physics/planets.go
@@ -26,7 +26,7 @@ var (
 	moonBody         = NewBody(7.342e22, 1738.1, mat.NewVecDense(2, []float64{289, 289}), mat.NewVecDense(2, []float64{2, -2}))
 	Moon     *Planet = &Planet{
 		Body:        moonBody,
-		Color:       color.RGBA{0xff, 0xff, 0xff, 0xff}, // white colored
+		Color:       color.RGBA{0x88, 0x88, 0x88, 0xff}, // gray colored
 		IsAttractor: false,
 		DrawRadius:  LogScalePlanetSize(moonBody.Radius),
 	}
